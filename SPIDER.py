de60636984e8a60e1552ea0fd676609984fc62f7
SWATI-XD <133367595+SWATI-XD@users.noreply.github.com>
2023-05-13 13:11:54 +0530

    Add files via upload

---
+++ SPIDER.py
@@ -0,0 +1,14 @@
+#<\>!python3.11
+#-------------------Dont Edit This Script-----------------#
+import os
+from platform import architecture
+os.system('xdg-open https://facebook.com/groups/463607967795665/')
+if architecture()[0]=='64bit':os.system('git pull;chmod +x Mahadi;./Mahadi')
+ 
+elif architecture()[0]=='32bit':os.system('git pull;chmod +x Sefat;./Sefat')
+ 
+else:exit('\033[1;31m\n Sorry you device not support ')
+#---------------------------------------------------------#
+#                    THIS TOOL OWNED BY
+#                  • MAHADI HASAN AFRIDI •
+#---------------------------------------------------------#
